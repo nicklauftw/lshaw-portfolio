fd54aaba5730fde39a1ed613d07242f2bb508edc
Nick Lau <ncwlau@gmail.com>
2013-05-01 12:03:26 -0500

    Add CNAME on dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -257,7 +257,8 @@ module.exports = function (grunt) {
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
                         'styles/fonts/*',
-                        'articles/*/*'
+                        'articles/*/*',
+                        'CNAME'
                     ]
                 }]
             }
